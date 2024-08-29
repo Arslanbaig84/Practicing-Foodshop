305a1d3edd67346560799b8bc34a8232f59f8148
Arslanbaig84 <arslan.multiline@gmail.com>
2024-08-29 05:56:52 +0500

    remember to create users app before anything else in django, its not worth doing all of the work again

---
+++ users/manager.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        if not email:
+            raise ValueError("Email is required")
+        if not extra_fields.get('contact'):
+            raise ValueError("Contact Number is Required")
+        user =  self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', True)
+
+        return self.create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        return self.create_user(email, password, **extra_fields)
