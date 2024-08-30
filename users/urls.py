81d679da63896833c164e9322a08b5d6b5eeee82
Arslanbaig84 <arslan.multiline@gmail.com>
2024-08-30 15:11:46 +0500

    login, logout routes done, register done

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("register", views.register, name='register'),
-    path("login_user", views.login_user, name='login_user')
+    path("login_user", views.login_user, name='login_user'),
+    path("logout_user", views.logout_user, name='logout_user')
     ]
