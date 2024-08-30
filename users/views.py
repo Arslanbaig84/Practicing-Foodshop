3636a955babd9dbbb44150a81549f3f3c7226d86
Arslanbaig84 <arslan.multiline@gmail.com>
2024-08-30 06:49:05 +0500

    complete register route for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserForm
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = CustomUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('register')
+    form = CustomUserForm()
+    return render(request, 'users/register.html', {'form':form})
