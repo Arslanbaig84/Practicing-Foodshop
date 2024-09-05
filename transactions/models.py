d6fc3932268ca983fcdbd55ca1a3172b2cdc2975
Arslanbaig84 <arslan.multiline@gmail.com>
2024-09-05 06:12:53 +0500

    minor changes in order models

--- transactions/models.py
+++ transactions/models.py
@@ -36,8 +36,9 @@ class Order(models.Model):
         ('refused', 'Refused'),
         ('returned', 'Returned'),
     ]
-
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -45,7 +46,7 @@ class Order(models.Model):
     shipping_address = models.TextField()  # add more detailed fields as needed
 
     def __str__(self):
-        return f"Order {self.id} - {self.user.email}"
+        return f"Order {self.uid} - {self.user.email}"
     
 
 class OrderItem(models.Model):
