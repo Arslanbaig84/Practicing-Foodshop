d815d2fbd45f6f6fdd2f32ef5b766243a3a92438
Arslanbaig84 <arslan.multiline@gmail.com>
2024-09-06 15:56:15 +0500

    added product editing

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("product_form", views.product_form, name="product_form"),
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("edit_product", views.edit_product, name="edit_product")
 ]
