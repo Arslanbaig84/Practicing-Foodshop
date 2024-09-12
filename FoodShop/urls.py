c2a2e0afae84c64a4d0dc95ddf2ca99d1b201acf
Arslanbaig84 <arslan.multiline@gmail.com>
2024-09-12 11:41:25 +0500

    added home app or homepage

--- FoodShop/urls.py
+++ FoodShop/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('products/', include('products.urls')),
     path('users/', include('users.urls')),
     path('transactions/', include('transactions.urls'))
